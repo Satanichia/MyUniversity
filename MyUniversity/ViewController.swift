3c3063b3fa7b532c546cf7488b2b8e0e5a24507b
Satanichia <Sakilia.Hallon@gmail.com>
2018-05-13 14:24:00 +0800

    First commit

---
+++ MyUniversity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyUniversity
+//
+//  Created by Satanichia on 2018/5/13.
+//  Copyright © 2018年 Jxaummd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
