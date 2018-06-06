3feb837abb6d8f8cf0dc1a14f97553ccaa8ab931
Satanichia <satanichia@lbc-ether.lan>
2018-06-06 12:55:38 +0800

    some additional data 2

--- MyUniversity/Constants/Constant.swift
+++ MyUniversity/Constants/Constant.swift
@@ -7,26 +7,58 @@
 //
 
 /* 网络接口地址*/
-//根位置地址
+
 let URL_HOME = "http://jxaummd.com"
+let URL_ENTRANT = "\(URL_HOME)/myUniversity/entrant"
+let URL_ENTRANT_HOME = "\(URL_ENTRANT)/homepage"
+let URL_ENTRANT_QA = "\(URL_ENTRANT)/answerqusetion"
+
+
 
 /* 新生专区 */
 
-/* 程序启动 */
-//启动页 e.g. http://jxaummd.com/myUniversity/entrant/homepage/startpage?id=1
-let URL_LAUNCH_IMAGE = "\(URL_HOME)/myUniversity/entrant/homepage/startpage"
 
-//更新引导页 e.g. http://jxaummd.com/myUniversity/entrant/homepage/bootpage?id=1
-let URL_LAUNCH_UPDATE = "\(URL_HOME)/myUniversity/entrant/homepage/bootpage"
+/* 用户操作 */
 
-//登陆接口 e.g. http://jxaummd.com/myUniversity/entrant/login?phoneNumber=12300000000&password=ABCDEFGH
+/* 登陆验证 e.g. /login?phoneNumber=#PARAM1#&password=#PARAM2#
+ * @type: GET
+ * @param(2): phoneNumber(as username), password
+ */
+let URL_ENTRANT_USER_LOGIN = "\(URL_ENTRANT)/login"
 
-//首页Banner e.g. http://jxaummd.com/myUniversity/entrant/homepage/homepage_banner?id=1
-let URL_ENTRANT_HOME_BANNER = "\(URL_HOME)/myUniversity/entrant/homepage/homepage_banner"
+/* 注册登记 e.g. /register?phoneNumber=#PARAM1#&nickname=#PARAM2#&password=#PARAM3#&code=#PARAM4#
+ * @type: GET
+ * @param(4): phoneNumber(as username), nickname, password, code(SMS verify code)
+ */
+let URL_ENTRANT_USER_REGISTER = "\(URL_ENTRANT)/register"
 
-//首页推送 e.g. http://jxaummd.com/myUniversity/entrant/homepage/homepage_propelling?offSet=1&pageSize=10
-let URL_ENTRANT_HOME_PUSH = "\(URL_HOME)/myUniversity/entrant/homepage/homepage_propelling"
+/* 发送验证码 e.g. /sendmessage?phoneNumber=13367925359
+ * @type: GET
+ * @param(1): phoneNumber
+ */
+let URL_ENTRANT_USER_VERIFY = "\(URL_ENTRANT)/sendmessage"
 
-//首页文章增加浏览(no respond) e.g. http://jxaummd.com/myUniversity/entrant/homepage/addhomepagepropellingview?propelling_id=1
-let URL_ENTRANT_HOME_PUSH_VCNT_ADD = "\(URL_HOME)/myUniversity/entrant/homepage/addhomepagepropellingview"
+
+/* 首页 /homepage */
+
+/* 启动页 e.g. /startpage?id=1
+ * @type: GET
+ * @param(1): id
+ */
+let URL_LAUNCH_IMAGE = "\(URL_ENTRANT_HOME)/startpage"
+
+/* 更新引导页 e.g. /bootpage?id=1
+ * @type: GET
+ * @param(1): id
+ */
+let URL_LAUNCH_UPDATE = "\(URL_ENTRANT_HOME)/bootpage"
+
+//首页Banner e.g. /homepage_banner?id=1
+let URL_ENTRANT_HOME_BANNER = "\(URL_ENTRANT_HOME)/homepage_banner"
+
+//首页推送 e.g. /homepage_propelling?offSet=1&pageSize=10
+let URL_ENTRANT_HOME_PUSH = "\(URL_ENTRANT_HOME)/homepage_propelling"
+
+//文章增加浏览(no respond) e.g. /addhomepagepropellingview?propelling_id=1
+let URL_ENTRANT_HOME_PUSH_VCNT_ADD = "\(URL_ENTRANT_HOME)/addhomepagepropellingview"
 
